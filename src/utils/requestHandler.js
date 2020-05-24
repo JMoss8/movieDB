29c2a5c69d0cd0cf595f4f726641a50010202f4b
Ján Mach <mach.jano@gmail.com>
2020-05-24 20:22:35 +0200

    API request handlers

---
+++ src/utils/requestHandler.js
@@ -0,0 +1,33 @@
+/**
+ * Basic request handler with default error handling.
+ *
+ * @param makeRequest:    request that should be taken care of, function that returns promise
+ * @param onNokResponse:  JSON with error codes (404, 500 etc.) as keys for appropriate error handling functions
+ */
+export const requestHandler = async (makeRequest, onNokResponse, onFinally) => {
+  try {
+    const response = await makeRequest()
+    if (!response.ok) {
+      (onNokResponse?.[response.status]) ? (
+        onNokResponse[response.status](response)
+      ) : (
+        defaultErrorHandler(response.status + ' ' + response.statusText)
+      )
+      return response
+    }
+
+    return response.json()
+  } catch (error) {
+    defaultErrorHandler(error)
+  } finally {
+    if (onFinally) {
+      onFinally()
+    }
+  }
+}
+
+export const defaultErrorHandler = error => {
+  console.warn(error)
+}
+
+export default requestHandler
