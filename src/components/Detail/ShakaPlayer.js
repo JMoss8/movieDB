b68d94360d384134bbb4b57ecac120248b81058e
Ján Mach <mach.jano@gmail.com>
2020-05-29 22:11:41 +0200

    ShakaPlayer ui

--- src/components/Detail/ShakaPlayer.js
+++ src/components/Detail/ShakaPlayer.js
@@ -1,28 +1,33 @@
 import React, {useEffect, useRef} from 'react'
-import shaka from 'shaka-player'
+import shaka from 'shaka-player/dist/shaka-player.ui'
+import 'shaka-player/dist/controls.css'
 import muxjs from 'mux.js'
 
-const initApp = (videoRef, manifestUri) => {
+const initApp = (videoRef, videoContainerRef, manifestUri) => {
   // Install built-in polyfills to patch browser incompatibilities.
   shaka.polyfill.installAll()
 
   // Check to see if the browser supports the basic APIs Shaka needs.
   if (shaka.Player.isBrowserSupported()) {
     // Everything looks good!
-    initPlayer(videoRef, manifestUri)
+    initPlayer(videoRef, videoContainerRef, manifestUri)
   } else {
     // This browser does not have the minimum set of APIs we need.
     console.error('Browser not supported!')
   }
 }
 
-const initPlayer = (videoRef, manifestUri) => {
+const initPlayer = (videoRef, videoContainerRef, manifestUri) => {
   // noinspection JSConstantReassignment TODO
   window.muxjs = muxjs
 
   // Create a Player instance.
   const video = videoRef.current
   const player = new shaka.Player(video)
+
+  const controlPanelElements = ['time_and_duration', 'spacer', 'mute', 'volume', 'fullscreen', 'overflow_menu']
+  const ui = new shaka.ui.Overlay(player, videoContainerRef.current, video)
+  ui.configure({controlPanelElements})
 
   // Listen for error events.
   player.addEventListener('error', onErrorEvent)
@@ -45,19 +50,22 @@ function onError(error) {
   console.error('Error code', error.code, 'object', error)
 }
 
-const ShakaPlayer = ({src, autoPlay = true, controls = true}) => {
+const ShakaPlayer = ({src, autoPlay = true}) => {
   const videoRef = useRef()
+  const videoContainerRef = useRef()
 
-  useEffect(() => initApp(videoRef, src), [src])
+  useEffect(() => initApp(videoRef, videoContainerRef, src), [src])
 
   return (
-    <video
-      ref={videoRef}
-      width='640'
-      poster='//shaka-player-demo.appspot.com/assets/poster.jpg'
-      autoPlay={autoPlay}
-      controls={controls}
-    />
+    <div ref={videoContainerRef}>
+      <video
+        ref={videoRef}
+        poster='//shaka-player-demo.appspot.com/assets/poster.jpg'
+        autoPlay={autoPlay}
+        controls={false}
+        style={{width: '100vw', height: '100vh'}}
+      />
+    </div>
   )
 }
 
