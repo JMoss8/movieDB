944865cbd84ade0c06541c9f059300f4fc95ef25
Ján Mach <mach.jano@gmail.com>
2020-05-31 21:47:25 +0200

    AliceCarousel prepared

--- src/modules/Carousel.js
+++ src/modules/Carousel.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
+
 const Carousel = ({title, items}) => (
   <div>
     <h2>{title}</h2>
 
     {items.results ? (
-      items.results.map(item => (
-        <Link key={item.id} to={`/detail/${item.media_type}/${item.id}`}>
-          {item.title ?? item.name}<br/>
-        </Link>
-      ))
+      <AliceCarousel
+        mouseTrackingEnabled
+        dotsDisabled
+        responsive={{0: {items: 5}}}
+      >
+        {items.results.map(item => (
+          <Link key={item.id} to={`/detail/${item.media_type}/${item.id}`}>
+            {item.title ?? item.name}<br/>
+          </Link>
+        ))}
+      </AliceCarousel>
     ) : items.loading ? (
       'loading...'
     ) : (
